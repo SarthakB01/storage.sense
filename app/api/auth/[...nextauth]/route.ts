ab4f92c1471c9149f061d7b024d0515c4975d6b2
Sarthak <sarthakbhagwat88@gmail.com>
2025-03-26 19:28:10 +0530

    try vercel now #1

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,8 @@
 import { authOptions } from './alternateroute'; // Update the path accordingly
 import NextAuth from 'next-auth';
 
+
+export { authOptions } from './alternateroute';
+
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST }
